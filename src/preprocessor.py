4d7ca37f89d9c8ab13175ed171706dfa56f52717
Macarena Fernandez Urquiza <m.fernandezurquiza@gmail.com>
2023-06-28 09:14:59 -0300

    add fuction for selecting relevant texts

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,5 +1,6 @@
 import os
 import re
+from typing import List
 from pdfminer.layout import LAParams
 from pdfminer.high_level import extract_text
 
@@ -14,13 +15,7 @@ def convert_pdf2txt(path: str, out_file: str=None) -> str:
 
 def remove_header(text: str) -> str:
     header = re.compile(
-        r'''(
-            29\/30 de diciembre de 2020|                # date
-            Sesión especial|                            # special session
-            “2020 \- Año del General Manuel Belgrano”|  # commemoration
-            Pág\. \d+                                   # page
-            )''',
-        re.X
+        r'''(29\/30 de diciembre de 2020|Sesión especial|\“2020 \- Año del General Manuel Belgrano\”|Pág\. \d+)'''
     )
     return header.sub('', text)
 
@@ -40,12 +35,77 @@ def split_attendees(text: str) -> str:
 
 def extract_attendees(text: str) -> str:
     attendees, text = split_attendees(text)
-    return text
+    return attendees, text
     
 
 def remove_special_chars(text: str, *args) -> str:
     chars = re.compile(r'[]')
     return chars.sub('', text)
+
+
+def find_roles(texts: List[str]):
+    idx, roles = list(), list()
+    for i, line in enumerate(texts):
+        if ':' in line:
+            rol = re.search(r'(.*?)\:', line).group(1)
+            roles.append(rol)
+            idx.append(i)
+    return idx, roles
+
+def postprocess_name(text: str) -> str:
+    title_pattern = re.compile(r'(DOCTOR|SENADOR|SEÑOR)A?')
+    name = title_pattern.sub('', text)
+    if ',' in name:
+        lastname, firstname = re.split(r'\,', name)
+        name = re.sub(r'\s+', ' ', f'{firstname} {lastname}')
+    return name.strip().title()
+
+def postprocess_attendees(text: str) -> str:
+    lines = text.split('\n')
+    roles_idx, roles = find_roles(lines)
+    output_file = os.path.join('data', 'attendees.csv')
+    f = open(output_file, 'w')
+    for line in lines[min(roles_idx):]:
+        idx = lines.index(line)
+        line = line.strip()
+        if line != '':
+            if ':' in line:
+                rol, name = line.split(':')
+                if name != '':
+                    _ = f.write(f'{rol}|{postprocess_name(name)}\n')
+            else:
+                next_roles = [i for i in roles_idx if i > idx]
+                rol_id = min(next_roles) if next_roles else max(roles_idx)
+                rol_pos = roles_idx.index(rol_id)
+                rol = roles[rol_pos-1] if next_roles else roles[rol_pos]
+                if re.search(r'\bY\b', line):
+                    names = re.split(r'[Y,]', line)
+                    for name in names:
+                        if name.strip() != '':
+                            _ = f.write(f'{rol}|{postprocess_name(name)}\n')
+                else:
+                    _ = f.write(f'{rol}|{postprocess_name(line)}\n')
+    return output_file
+
+
+def select_section(text: str):
+    section = re.compile(
+        r"6\.\s+ Regulación del acceso a la interrupción voluntaria del embarazo y a la \n+atención postaborto \(O\.D\. N\° 716\/20\.\)(.*)8\.\s+Apéndice",
+        re.DOTALL
+    )
+    section_text = section.search(text)
+    if section_text:
+        return section_text.group(1).strip()
+    else:
+        return None
+
+def speaker(text):
+    #speaker = re.compile(r"((Sra?\.)?.*?\.\-)", re.I)
+    text = re.sub(r"(\n\s?)+", r"\n", text)
+    speaker = re.compile(r"([^\n].*?\.\-)", re.I)
+    speakers = speaker.split(text)
+    with open("data/speakers.txt","w") as f:
+        _ = f.write(f"\n{'-'*80}\n".join(speakers))
 
 
 def preprocess(path: str, out_file: str=None) -> str:
@@ -55,7 +115,13 @@ def preprocess(path: str, out_file: str=None) -> str:
     text = remove_special_chars(text)
     text = remove_header(text)
     text = remove_footer(text)
-    text = extract_attendees(text)
+    attendees, text = extract_attendees(text)
+    _ = postprocess_attendees(attendees)
+    text = select_section(text)
+    speaker(text)
     with open(out_file, 'w') as f:
         _ = f.write(text)
+    
 
+preprocess('data/session_29-12-2020.txt')
+#<?xml version="1.0" encoding="UTF-8"?>
