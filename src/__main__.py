c518d489382f466ad49033b262da0fac643340d7
Macarena Fernandez Urquiza <m.fernandezurquiza@gmail.com>
2023-07-09 22:11:33 -0300

    add downloader for senators info

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,9 @@
 import argparse
 
-from src.downloader import download
 from src.config_hander import ConfigHandler
+from src.downloaders.data import download as download_data
 from src.preprocessor.pipeline import convert_pdf2txt, preprocess
+from src.downloaders.attendees import download as download_attendees
 
 
 CONFIG = 'config.json'
@@ -11,12 +12,15 @@ config = ConfigHandler(CONFIG)
 parser = argparse.ArgumentParser()
 subparser = parser.add_subparsers(dest='action', required=True)
 subparser_download = subparser.add_parser('download')
-subparser_download.add_argument('data', choices=['biblio', 'data'])
+subparser_download.add_argument('data', choices=['biblio', 'data', "attendees"])
 args = parser.parse_args()
 
 if args.action == 'download':
     if args.data == 'data':
         data_config = config.get(args.data)
-        pdf_out = download(**data_config)
+        pdf_out = download_data(**data_config)
         txt_out = convert_pdf2txt(pdf_out)
         csv_out, xml_out = preprocess(txt_out)
+    elif args.data == 'attendees':
+        data_config = config.get(args.data)
+        download_attendees(**data_config)
