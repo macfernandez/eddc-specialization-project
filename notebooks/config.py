91b3833c17790d5693ed06b6df0d92a588b06fce
Macarena Fernández Urquiza <m.fernandezurquiza@gmail.com>
2024-07-29 01:27:19 -0300

    models wip

--- notebooks/config.py
+++ notebooks/config.py
@@ -7,5 +7,6 @@ HERE = os.getcwd()
 PROJECT_PATH = os.path.dirname(HERE)
 DATA_PATH = os.path.join(PROJECT_PATH, "data")
 VISUALIZATIONS_PATH = os.path.join(PROJECT_PATH, "visualizations")
+MODELS_PATH = os.path.join(PROJECT_PATH, "models")
 
 sns.set_style("whitegrid")
