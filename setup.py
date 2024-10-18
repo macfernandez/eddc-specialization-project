2f236e1338cd158ce3718c25b8f1f9655cf0d374
Macarena Fernández Urquiza <m.fernandezurquiza@gmail.com>
2024-10-17 21:44:09 -0300

    add extra requires

--- setup.py
+++ setup.py
@@ -10,17 +10,29 @@ setup(name='eddc',
       url='https://github.com/macfernandez/eddc-specialization-project',
       packages=['src'],
       install_requires=[
+        'bs4==0.0.1',
         'jinja2==3.1.2',
+        'lxml==4.9.3',
         'pandas==2.0.3',
         'pdfminer==20191125',
         'pdfminer-six==20221105',
         'requests==2.28.2',
-        'selenium==4.10.0'
+        'selenium==4.10.0',
+        'webdriver-manager==4.0.1'
       ],
       extras_require={
         "dev":[
-            'pytest==7.2.1',
-            'pytest-cov==4.0.0'
+          'pytest==7.2.1',
+          'pytest-cov==4.0.0'
+        ],
+        "nb":[
+          'inflection==0.5.1',
+          'joblib==1.4.2',
+          'nltk==3.8.1',
+          'scikit-learn==1.3.0',
+          'seaborn==0.12.2',
+          'spacy==3.7.4',
+          'es-core-news-md @ https://github.com/explosion/spacy-models/releases/download/es_core_news_md-3.7.0/es_core_news_md-3.7.0-py3-none-any.whl',
         ]
       }
      )
