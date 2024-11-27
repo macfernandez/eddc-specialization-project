9f473e3a68b9bcfd400dca64de8c100ee2fa92d0
Macarena Fernández Urquiza <m.fernandezurquiza@gmail.com>
2024-11-27 14:23:16 +0000

    fix dev container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,6 @@
-git config --global http.sslverify false
-git config --global --add safe.directory /workspace
-
-python -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-
-python -m src download data
+#python -m venv venv
+#source venv/bin/activate
+#pip install --upgrade pip
+#pip install -r requirements.txt
+#
+#python -m src download data
